24869f3c6e0f82cb96c08d84dd52ec3603d3c437
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-29 12:59:56 -0700

    Add GitHub client ID query

--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -1,4 +1,10 @@
-import { Resolver } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
+import { GITHUB_CLIENT_ID } from "../config";
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  @Query(() => String)
+  gitHubClientId() {
+    return GITHUB_CLIENT_ID;
+  }
+}
