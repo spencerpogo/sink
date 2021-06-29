69d11be4e0f79dbad9590a9e367588d2e8843930
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-29 13:52:39 -0700

    Implement express sessions with connect-redis

--- api/src/config.ts
+++ api/src/config.ts
@@ -2,10 +2,19 @@ import * as env from "env-var";
 
 export const HOST: string = env.get("HOST").default("127.0.0.1").asString();
 export const PORT: number = env.get("PORT").default(8080).asPortNumber();
+export const SESSION_SECRET = env.get("SESSION_SECRET").required().asString();
 export const DATABASE_URL: string = env
   .get("DATABASE_URL")
   .required()
   .asUrlString();
+export const REDIS_HOST: string = env
+  .get("REDIS_HOST")
+  .default("127.0.0.1")
+  .asString();
+export const REDIS_PORT: number = env
+  .get("REDIS_PORT")
+  .default(6379)
+  .asPortNumber();
 export const GITHUB_CLIENT_ID: string = env
   .get("GITHUB_CLIENT_ID")
   .required()
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,29 @@
 import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
 import express from "express";
+import session from "express-session";
+import IORedis from "ioredis";
 import path from "path";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
-import { DATABASE_URL, HOST, PORT } from "./config";
+import {
+  DATABASE_URL,
+  HOST,
+  PORT,
+  REDIS_HOST,
+  REDIS_PORT,
+  SESSION_SECRET,
+} from "./config";
 import { User } from "./models/User";
 import { HelloResolver } from "./resolvers/hello";
 import { UserResolver } from "./resolvers/user";
 
+const RedisStore = connectRedis(session);
+
 async function main() {
+  const app = express();
+
   const conn = await createConnection({
     type: "postgres",
     url: DATABASE_URL,
@@ -19,12 +33,26 @@ async function main() {
   });
   await conn.runMigrations();
 
+  const redis = new IORedis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  });
+  app.use(
+    session({
+      store: new RedisStore({ client: redis }),
+      saveUninitialized: false,
+      secret: SESSION_SECRET,
+      resave: false, // redis supports touch
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+      },
+    })
+  );
+
   const schema = await buildSchema({
     resolvers: [HelloResolver, UserResolver],
   });
   const server = new ApolloServer({ schema });
-
-  const app = express();
   server.applyMiddleware({ app });
 
   app.listen({ host: HOST, port: PORT }, () =>
