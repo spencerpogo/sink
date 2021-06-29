492ad0a7f6c5d69f21cd4050d396b56435d5363f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-29 13:11:31 -0700

    Implement genGitHubLoginURL

--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -1,10 +1,21 @@
-import { Query, Resolver } from "type-graphql";
+import * as querystring from "querystring";
+import { Arg, Query, Resolver } from "type-graphql";
 import { GITHUB_CLIENT_ID } from "../config";
+
+const GITHUB_URL = "https://github.com";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
-  gitHubClientId() {
-    return GITHUB_CLIENT_ID;
+  genGitHubLoginURL(@Arg("redirectUri") redirectUri: string) {
+    return (
+      `${GITHUB_URL}/login/oauth/authorize?` +
+      querystring.stringify({
+        client_id: GITHUB_CLIENT_ID,
+        redirect_uri: redirectUri,
+        scope: "", // no scope gives access to public information
+        state: "TODO", // TODO: generate uuid and store it in session
+      })
+    );
   }
 }
