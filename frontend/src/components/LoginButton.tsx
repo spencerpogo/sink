8f805ff0617d12913c01b5af1099679c0b9e4b6b
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-29 17:49:38 -0700

    Improve LoginButton design

--- frontend/src/components/LoginButton.tsx
+++ frontend/src/components/LoginButton.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Icon } from "@chakra-ui/react";
+import { GoMarkGithub } from "react-icons/go";
 
 export function LoginButton() {
-  return <Button>Login</Button>;
+  return (
+    <Button size="lg" leftIcon={<Icon as={GoMarkGithub} w={8} h={8} />}>
+      Continue with GitHub
+    </Button>
+  );
 }
 
 export default LoginButton;
