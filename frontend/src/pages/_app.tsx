4f19533d1e3276f68cd0ed2cc8d2f236f677be35
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-29 18:08:03 -0700

    Add apollo client to frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,19 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { Chakra } from "../chakra";
 
+const client = new ApolloClient({
+  uri: process.env.NODE_PUBLIC_API_URL,
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Chakra cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </Chakra>
+    <ApolloProvider client={client}>
+      <Chakra cookies={pageProps.cookies}>
+        <Component {...pageProps} />
+      </Chakra>
+    </ApolloProvider>
   );
 }
 
