9d2ac5c53b32e1762f3256a3687748739f4f6112
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-07-01 18:15:00 -0700

    Use me query on home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,17 @@
+import { Text } from "@chakra-ui/react";
 import Landing from "../components/Landing";
+import { useMeQuery } from "../generated/graphql";
 
 export function Index() {
+  const { data, loading, error } = useMeQuery();
+
+  if (loading) return <></>;
+  if (error) return <Text>Error: {String(error.message)}</Text>;
+
+  if (data && data.me.name) {
+    return <Text>Hello {data.me.name}</Text>;
+  }
+
   return <Landing />;
 }
 
