c322c8f7279133c191fb19286d27d976e60f6d7a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-01-07 16:59:26 -0800

    Refactor: separate GitHub specific code

---
+++ api/src/githubAuth.ts
@@ -0,0 +1,85 @@
+import fetch, { Response } from "node-fetch";
+import { stringify as querystringify } from "querystring";
+import { version as serverVersion } from "../package.json";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "./config.js";
+
+const GITHUB_URL = "https://github.com";
+const GITHUB_API_URL = "https://api.github.com";
+
+const ACCEPT_JSON = { Accept: "application/json" };
+const UA = {
+  "User-Agent": `sink/${serverVersion} https://github.com/Scoder12/sink`,
+};
+
+export class LoginError extends Error {}
+
+export async function validateResponse(
+  r: Response,
+  remote: string,
+  method: string
+): Promise<void> {
+  if (r.status != 200) {
+    const error = `Got status ${r.status} ${r.statusText} from ${remote} during ${method}`;
+    let text = "";
+    try {
+      text = await r.text();
+    } catch (e) {
+      text = e;
+    }
+    console.error({ error, text });
+    throw new LoginError(error);
+  }
+}
+
+export async function tokenFromCode(
+  githubCode: string
+): Promise<{ token: string }> {
+  const r = await fetch(GITHUB_URL + "/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...ACCEPT_JSON,
+      ...UA,
+    },
+    body: JSON.stringify({
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+      code: githubCode,
+    }),
+  });
+
+  await validateResponse(r, "GitHub", "login");
+
+  const data = await r.json();
+  if (typeof data !== "object" || !data.access_token) {
+    const error = "Unexpected response from GitHub";
+    console.error({ error, data });
+    throw new LoginError(error);
+  }
+  return { token: data.access_token };
+}
+
+export async function getGitHubInfo(
+  token: string
+): Promise<{ name: string; githubId: number }> {
+  const r = await fetch(GITHUB_API_URL + "/user", {
+    headers: { Authorization: `token ${token}`, ...ACCEPT_JSON, ...UA },
+  });
+  await validateResponse(r, "GitHub", "fetch user info");
+  const { login, id } = await r.json();
+  if (!login || !id)
+    throw new LoginError("Received invalid user data from GitHub");
+  return { name: login, githubId: id };
+}
+
+export function genGithubLoginURL(redirectUri: string, state: string) {
+  return (
+    `${GITHUB_URL}/login/oauth/authorize?` +
+    querystringify({
+      client_id: GITHUB_CLIENT_ID,
+      redirect_uri: redirectUri,
+      scope: "", // no scope gives access to public information
+      state,
+    })
+  );
+}
--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -1,5 +1,3 @@
-import fetch, { Response } from "node-fetch";
-import * as querystring from "querystring";
 import {
   Arg,
   Ctx,
@@ -11,19 +9,15 @@ import {
   UseMiddleware,
 } from "type-graphql";
 import { v4 as uuidv4 } from "uuid";
-import { version as serverVersion } from "../../package.json";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "../config";
+import {
+  genGithubLoginURL,
+  getGitHubInfo,
+  LoginError,
+  tokenFromCode,
+} from "../githubAuth.js";
 import { isAuth } from "../middleware/isAuth";
 import { User } from "../models/User";
 import { MyContext } from "../types";
-
-const GITHUB_URL = "https://github.com";
-const GITHUB_API_URL = "https://api.github.com";
-
-const ACCEPT_JSON = { Accept: "application/json" };
-const UA = {
-  "User-Agent": `sink/${serverVersion} https://github.com/Scoder12/sink`,
-};
 
 @ObjectType()
 class LoginResponse {
@@ -32,65 +26,6 @@ class LoginResponse {
 
   @Field(() => User, { nullable: true })
   user?: User;
-}
-
-class LoginError extends Error {}
-
-async function validateResponse(
-  r: Response,
-  remote: string,
-  method: string
-): Promise<void> {
-  if (r.status != 200) {
-    const error = `Got status ${r.status} ${r.statusText} from ${remote} during ${method}`;
-    let text = "";
-    try {
-      text = await r.text();
-    } catch (e) {
-      text = e;
-    }
-    console.error({ error, text });
-    throw new LoginError(error);
-  }
-}
-
-async function tokenFromCode(githubCode: string): Promise<{ token: string }> {
-  const r = await fetch(GITHUB_URL + "/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      ...ACCEPT_JSON,
-      ...UA,
-    },
-    body: JSON.stringify({
-      client_id: GITHUB_CLIENT_ID,
-      client_secret: GITHUB_CLIENT_SECRET,
-      code: githubCode,
-    }),
-  });
-
-  await validateResponse(r, "GitHub", "login");
-
-  const data = await r.json();
-  if (typeof data !== "object" || !data.access_token) {
-    const error = "Unexpected response from GitHub";
-    console.error({ error, data });
-    throw new LoginError(error);
-  }
-  return { token: data.access_token };
-}
-
-async function getGitHubInfo(
-  token: string
-): Promise<{ name: string; githubId: number }> {
-  const r = await fetch(GITHUB_API_URL + "/user", {
-    headers: { Authorization: `token ${token}`, ...ACCEPT_JSON, ...UA },
-  });
-  await validateResponse(r, "GitHub", "fetch user info");
-  const { login, id } = await r.json();
-  if (!login || !id)
-    throw new LoginError("Received invalid user data from GitHub");
-  return { name: login, githubId: id };
 }
 
 @Resolver()
@@ -102,15 +37,7 @@ export class UserResolver {
   ) {
     const state = uuidv4();
     req.session.authState = state;
-    return (
-      `${GITHUB_URL}/login/oauth/authorize?` +
-      querystring.stringify({
-        client_id: GITHUB_CLIENT_ID,
-        redirect_uri: redirectUri,
-        scope: "", // no scope gives access to public information
-        state,
-      })
-    );
+    return genGithubLoginURL(redirectUri, state);
   }
 
   @Mutation(() => LoginResponse)
