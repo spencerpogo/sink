ff98dcf0c9ac420752ec06d2a9f94a63cfa0ffca
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-01-23 15:36:06 -0800

    Fix incomplete migration

--- api/src/migrations/1642980631163-initial.ts
+++ api/src/migrations/1642980631163-initial.ts
@@ -1,9 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class initial1642979978704 implements MigrationInterface {
-    name = 'initial1642979978704'
+export class initial1642980631163 implements MigrationInterface {
+    name = 'initial1642980631163'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "githubId" integer NOT NULL, "name" varchar NOT NULL, CONSTRAINT "UQ_0d84cc6a830f0e4ebbfcd6381dd" UNIQUE ("githubId"), CONSTRAINT "UQ_065d4d8f3b5adb4a08841eae3c8" UNIQUE ("name"))`);
         await queryRunner.query(`CREATE TABLE "event" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "start" numeric NOT NULL, "userId" integer)`);
         await queryRunner.query(`CREATE TABLE "temporary_event" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "start" numeric NOT NULL, "userId" integer, CONSTRAINT "FK_01cd2b829e0263917bf570cb672" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
         await queryRunner.query(`INSERT INTO "temporary_event"("id", "name", "start", "userId") SELECT "id", "name", "start", "userId" FROM "event"`);
@@ -17,6 +18,7 @@ export class initial1642979978704 implements MigrationInterface {
         await queryRunner.query(`INSERT INTO "event"("id", "name", "start", "userId") SELECT "id", "name", "start", "userId" FROM "temporary_event"`);
         await queryRunner.query(`DROP TABLE "temporary_event"`);
         await queryRunner.query(`DROP TABLE "event"`);
+        await queryRunner.query(`DROP TABLE "user"`);
     }
 
 }
