fae9324a68059d76387ce4b13f5e90a3ac4de4ec
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-04-05 21:43:23 -0700

    Add title tags

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "../Header";
 import CreateTask from "./CreateTask";
 import TasksList from "./TasksList";
@@ -5,6 +6,9 @@ import TasksList from "./TasksList";
 export default function Home({ name }: { name: string }) {
   return (
     <>
+      <Head>
+        <title>Sink</title>
+      </Head>
       <Header name={name} />
       <main className="mx-5">
         <div className="my-3">
--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -1,13 +1,19 @@
+import Head from "next/head";
 import LoginButton from "./LoginButton";
 
 export default function Landing() {
   return (
-    <div className="h-full flex justify-center items-center">
-      <div>
-        <h1 className="text-4xl font-bold">Sink</h1>
-        <p>Work-in-progress time-tracking app</p>
-        <LoginButton />
+    <>
+      <Head>
+        <title>Sink</title>
+      </Head>
+      <div className="h-full flex justify-center items-center">
+        <div>
+          <h1 className="text-4xl font-bold">Sink</h1>
+          <p>Work-in-progress time-tracking app</p>
+          <LoginButton />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
